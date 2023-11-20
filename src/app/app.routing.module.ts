d658016e9cc22c186d8503072411b1c17d023712
Laimonas Lizdenis <laimonas.lizdenis@devbridge.com>
2023-11-20 16:46:31 +0200

    Commit

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { routes } from "./app.routes";
 
-import { routes } from './app.routes';
 
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
-};
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
